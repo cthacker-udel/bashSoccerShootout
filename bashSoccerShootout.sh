8ec2302ef90b66852f8c37bf7655062a189f7cab
cthacker-udel <cthacker@udel>
2021-04-29 02:23:25 -0400

    Finalized soccer shootout functionality, added shootout overtime functionality as well

--- bashSoccerShootout.sh
+++ bashSoccerShootout.sh
@@ -19,6 +19,8 @@ playerscore=0
 computerscore=0
 totalkicksplayer=0
 totalkickscomputer=0
+overtime=0
+overtimerounds=1
 
 
 display_menu(){
@@ -74,8 +76,36 @@ while true; do
     fi
     while [ "$turn" -eq 1 -o "$turn" -eq 0 ]; do
         display_score
+        sleep 3
+        if [ "$overtime" -eq 1 -a "$totalkicksplayer" -eq "$totalkickscomputer" ]; then
+            if [ "$playerscore" -gt "$computerscore" ]; then
+                echo -e "\n$teamPlayer WINS OVERTIME!!!"
+                exit 0
+            elif [ "$playerscore" -lt "$computerscore" ]; then
+                echo -e "\n$teamComputer WINS OVERTIME!!!"
+                exit 0
+            else
+                echo -e "\nTIE after $overtimerounds of overtime!"
+                sleep 2
+                ((overtimerounds++))
+            fi
+        fi
+        if [ "$totalkicksplayer" -eq 5 -a "$totalkickscomputer" -eq 5 ]; then
+            if [ "$playerscore" -gt "$computerscore" ]; then
+                echo -e "\n $teamPlayer WINS!!"
+                exit 0
+            elif [ "$playerscore" -lt "$computerscore" ]; then
+                echo -e "\n $teamComputer WINS!!"
+                exit 0
+            else
+                echo -e "\n -----> ENTERING OVERTIME <-----"
+                sleep 3
+                overtime=1
+            fi
+        fi
         if [ "$turn" -eq 1 ]; then
             echo -e "\nPlayer taking shot"
+            ((totalkicksplayer++))
             display_menu
             read playerShot
             if [ "$playerShot" -lt 1 -o "$playerShot" -gt 3 ]; then
@@ -91,7 +121,7 @@ while true; do
                         sleep 2
                         turn=0
                     else
-                        echo "\n$teamPlayer SCORES!!"
+                        echo -e "\n$teamPlayer SCORES!!"
                         sleep 2
                         turn=0
                         ((playerscore++))
@@ -124,6 +154,7 @@ while true; do
             fi
                     
         else
+            ((totalkickscomputer++))
             echo -e "\nComputer choosing shot"
             computerChoice=$(shuf -i 1-3 -n 1)
             case $computerChoice in
